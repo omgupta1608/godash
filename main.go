a8c8a2b41abf1b758db88c631335b43727e653b8
Om Gupta <omgupta1608@gmail.com>
2024-04-26 22:06:10 +0530

    add: incomplete functions and comments

--- main.go
+++ main.go
@@ -1,48 +1,91 @@
 package godash
 
-import "reflect"
+import (
+	"reflect"
+)
 
-func Chunk[T any](arr []T, size *int) [][]T {
+// Returns an array of elements split into groups the length of size. If array can't be split evenly, the final chunk will be the remaining elements.
+func Chunk[T any](arr []T, size *int) (res [][]T) {
+	if arr == nil {
+		return nil
+	}
 	var DEFAULT_CHUNK_SIZE int = 1
-	if size == nil {
+	if size == nil || *size == 0 {
 		size = &DEFAULT_CHUNK_SIZE
 	}
-	return nil
-
-	// TODO
+	if len(arr) <= *size {
+		res = append(res, arr)
+		return res
+	}
+	i := 0
+	for len(arr)-(i+1) >= *size {
+		res = append(res, arr[i:i+*size])
+		i += *size
+	}
+	if i < len(arr) {
+		res = append(res, arr[i:])
+	}
+	return
 }
 
+// Returns a new array concatenating array with any additional values.
 func Concat[T any](arr []T, args ...T) []T {
-	// TODO
+	if arr == nil {
+		return nil
+	}
+	arr = append(arr, args...)
 	return arr
 }
 
+// Returns a new slice with n elements dropped from the beginning.
 func Drop[T any](arr []T, n *int) []T {
+	if arr == nil {
+		return nil
+	}
 	var DEFAULT_DROP_SIZE int = 1
 	if n == nil {
 		n = &DEFAULT_DROP_SIZE
 	}
-	return arr
 
-	// TODO
+	if *n >= len(arr) {
+		return []T{}
+	}
+	return arr[*n:]
 }
 
+// Returns a slice with n elements dropped from the end.
 func DropRight[T any](arr []T, n *int) []T {
+	if arr == nil {
+		return nil
+	}
 	var DEFAULT_DROP_SIZE int = 1
 	if n == nil {
 		n = &DEFAULT_DROP_SIZE
 	}
-	return arr
-
-	// TODO
+	if *n >= len(arr) {
+		return []T{}
+	}
+	return arr[:len(arr)-*n]
 }
 
+// Checks whether a value is present in the given array or not. Returns a boolean.
 func Includes[T any](arr []T, val T) bool {
-	// TODO
-	return true
+	if arr == nil {
+		return false
+	}
+	for _, v := range arr {
+		if reflect.DeepEqual(v, val) {
+			return true
+		}
+	}
+	return false
 }
 
+// Returns the index of the first element that matches val.
 func FindIndex[T any](arr []T, val T) int {
+	if arr == nil {
+		return -1
+	}
 	for i, v := range arr {
 		if reflect.DeepEqual(v, val) {
 			return i
@@ -51,6 +94,7 @@ func FindIndex[T any](arr []T, val T) int {
 	return -1
 }
 
+// Gets the first element of array. Returns nil with the array is empty or nil
 func Head[T any](arr []T) *T {
 	if len(arr) > 0 {
 		return &arr[0]
@@ -58,6 +102,7 @@ func Head[T any](arr []T) *T {
 	return nil
 }
 
+// Gets the last element of array.  Returns nil with the array is empty or nil
 func Last[T any](arr []T) *T {
 	if len(arr) > 0 {
 		return &arr[len(arr)-1]
@@ -65,6 +110,7 @@ func Last[T any](arr []T) *T {
 	return nil
 }
 
+// Gets all but the last element of array.
 func Initial[T any](arr []T) []T {
 	if len(arr) == 0 || len(arr) == 1 {
 		return []T{}
@@ -72,22 +118,24 @@ func Initial[T any](arr []T) []T {
 	return arr[:len(arr)-1]
 }
 
-func Intersection[T comparable](arr1 []T, arr2 []T) []T {
-	res := []T{}
-	v1 := make(map[T]bool)
-
-	for _, v := range arr1 {
-		v1[v] = true
+// Creates an array of unique values that are included in all given arrays.
+func Intersection[T comparable](arrays ...[]T) []T {
+	valCounts := map[T]int{}
+	for _, arr := range arrays {
+		for _, val := range arr {
+			valCounts[val]++
+		}
 	}
-
-	for _, v := range arr2 {
-		if _, ok := v1[v]; ok {
-			res = append(res, v)
+	res := []T{}
+	for k, v := range valCounts {
+		if v >= len(arrays) {
+			res = append(res, k)
 		}
 	}
 	return res
 }
 
+// Converts all elements in array into a string separated by separator.
 func Join(arr []string, sep string) (res string) {
 	for i, v := range arr {
 		res += v
@@ -97,4 +145,3 @@ func Join(arr []string, sep string) (res string) {
 	}
 	return
 }
-
